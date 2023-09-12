d071eda95400215c2fe648df42a0aeb8c1e2cbbc
JoynalAbedin3 <engr.joynal.bd@gmail.com>
2023-09-13 01:39:45 +0600

    Task-1

---
+++ personal_info/personal_info.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = "Joynal";
+$age = 25;
+$country = "Bangladesh";
+
+$brief = "I am a php laravel web developer. Recently I have completed my bsc in computer science 
+and engineering from green university of banladesh.
+Now I am trying my best to develop my coding skill as a web developer";
+
+echo "Name:$name"."\n"."Age:$age" ."\n"."Country:$country"."\n"."Introduction:$brief";
